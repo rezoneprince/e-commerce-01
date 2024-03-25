aa131b8f631eab19c3b2597c16a2968002e85063
rezone <rezoneprinceme@gmail.com>
2024-03-26 05:37:04 +0600

    header state work added

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import cart_icon from "../../assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -12,16 +13,15 @@ const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li>
-          Shop <hr />
-        </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => setMenu("shop")}>Shop{menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("mens")}>Men{menu === "mens" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("womens")}>Women{menu === "womens" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("kids")}>Kids{menu === "kids" ? <hr /> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
