9c658ce5adc53d0a4b9661efd14aa98011bda0a7
rezone <rezoneprinceme@gmail.com>
2024-03-26 03:56:28 +0600

    first commit

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import cart_icon from "../../assets/cart_icon.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>Shopper</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li>
+          Shop <hr />
+        </li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
